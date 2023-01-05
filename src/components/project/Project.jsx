d430814be410bdf4ec615d65e8d178139902955a
eruditeme <evangelineyiqin@gmail.com>
2023-01-05 06:12:28 -0500

    Added projects into slides

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -13,38 +13,34 @@ const Project = () => {
   return (
     <section id="projects">
       <h2 id='project-title'>Projects</h2>
-      <div className="project__container">
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <div class='project_item'>
+              <h2 class="project-name">Cozy Town</h2>
+              <p class="project-desc">
+                A game where the player can walk around cozy town and battle evil vegetables at
+                battle zones.
+              </p>
+              <p class="project-desc">Built using HTML Canvas</p>
+              <a href="https://github.com/eruditeme/cozyBattleGame" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
+              <a href="https://papaya-cendol-bd7840.netlify.app/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class='project_item'>
+              <h2 class="project-name">Cozy Town</h2>
+              <img src={ONE} alt="project 1" />
+              <p class="project-desc">
+                A game where the player can walk around cozy town and battle evil vegetables at
+                battle zones.
+              </p>
+              <p class="project-desc">Built using HTML Canvas</p>
+              <a href="https://www.github.com/" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
+              <a href="https://www.github.com/" className='btn' target="_blank" rel="noopener noreferrer">Live Demo</a>
+            </div>
+          </SwiperSlide>
           <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
         </Swiper>
-        <article className="project__item">
-          <h3>Cozy Town</h3>
-          <p>
-            A game where the player can walk around cozy town and battle evil vegetables at
-            battle zones.
-          </p>
-          <p>
-            Built using HTML Canvas
-          </p>
-          <a href="https://github.com/eruditeme/cozyBattleGame" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
-          <a href="https://papaya-cendol-bd7840.netlify.app/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
-        </article>
-        <article className="project__item">
-          <div className='project__item-image'>
-            <img src={ONE} alt="project 1" />
-          </div>
-          <a href="https://www.github.com/" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
-          <a href="https://www.github.com/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
-        </article>
-      </div>
     </section>
   )
 }
