11a845debc03eec01d9408e9bf8ecc02a1d6c3d3
eruditeme <evangelineyiqin@gmail.com>
2023-05-05 21:07:41 -0400

    updated project section

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -16,30 +16,32 @@ const Project = () => {
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
           <SwiperSlide>
             <div class='project_item'>
-              <h2 class="project-name">Town</h2>
+              <h2 class="project-name">Easy Chef</h2>
               <p class="project-desc">
-                A game created on HTML canvas where the player can walk 
-                around Cozy Town.
+                Easy Chef is a recipe sharing web application. 
+                With Easy Chef, you can search through millions of different recipes 
+                from various diets and ingredients from all over the world! You can 
+                also share your custom recipe with everyone, like and comment on recipes, 
+                as well as filter recipe ideas based on your needs! You don't need to worry 
+                about the shopping list either; Easy Chef takes care of that as well!
               </p>
-              <p class="project-desc">Built using HTML Canvas</p>
-              <a href="https://github.com/eruditeme/cozyBattleGame" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
-              <a href="https://papaya-cendol-bd7840.netlify.app/" className='btn' target="_blank" rel="noopener noreferrer">Live Demo</a>
+              <p class="project-desc">Built using Django, Node.js, React, SQL, HTML, CSS, Bootstrap</p>
+              <a href="https://github.com/eruditeme/easy-chef" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div class='project_item'>
-              <h2 class="project-name">Cozy Town</h2>
-              <img src={ONE} alt="project 1" />
+              <h2 class="project-name">Simon</h2>
               <p class="project-desc">
-                A game where the player can walk around cozy town and battle evil vegetables at
-                battle zones.
+                Simon is a game designed to test and improve a player's memory and cognitive skills. 
+                The game consists of a small electronic console with four colored buttons, 
+                each of which produces a unique sound when pressed.
               </p>
-              <p class="project-desc">Built using HTML Canvas</p>
-              <a href="https://www.github.com/" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
-              <a href="https://www.github.com/" className='btn' target="_blank" rel="noopener noreferrer">Live Demo</a>
+              <p class="project-desc">Built using HTML, CSS, jQuery (JavaScript)</p>
+              <a href="https://github.com/eruditeme/Simon-Game" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
+              <a href="https://eruditeme.github.io/Simon-Game/" className='btn' target="_blank" rel="noopener noreferrer">Live Demo</a>
             </div>
           </SwiperSlide>
-          <SwiperSlide>Slide 1</SwiperSlide>
         </Swiper>
     </section>
   )
