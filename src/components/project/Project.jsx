2019450ed6133d4493099b7e63c1633dbe16782b
eruditeme <evangelineyiqin@gmail.com>
2023-05-06 04:13:05 -0400

    edit easy chef description

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -11,9 +11,12 @@ const Project = () => {
             <h2 class="project-name">Easy Chef</h2>
             <p class="project-desc">
               Easy Chef is a recipe sharing web application. 
-              With Easy Chef, you can search through millions of different recipes 
-              from various diets and ingredients from all over the world. You don't need to worry 
-              about the shopping list either; Easy Chef takes care of that as well!
+              With Easy Chef, you can search through millions of different 
+              recipes from various diets and ingredients from all over the world, 
+              share your custom recipe with everyone, 
+              like and comment on recipes, as well as filter recipe ideas based 
+              on your needs! You don't need to worry about the shopping list either; 
+              Easy Chef takes care of that as well!
             </p>
             <p class="project-desc">Built using Django, Node.js, React, SQL, HTML, CSS, Bootstrap</p>
             <a href="https://github.com/eruditeme/easy-chef" className='btn' target="_blank" rel="noopener noreferrer">Github</a>
