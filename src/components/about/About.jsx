94c3bae4216e7ba418f055cffb9333b60fd4ddc7
eruditeme <evangelineyiqin@gmail.com>
2023-06-20 16:59:51 -0400

    added block animation

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -32,8 +32,27 @@ const About = () => {
               playing video games, and watching space documentaries. 
             </p>
           </div>
-          <div className='about__me-image'>
-              
+          <div class="loader">
+            <div class="box box-1">
+              <div class="side-left"></div>
+              <div class="side-right"></div>
+              <div class="side-top"></div>
+            </div>
+            <div class="box box-2">
+              <div class="side-left"></div>
+              <div class="side-right"></div>
+              <div class="side-top"></div>
+            </div>
+            <div class="box box-3">
+              <div class="side-left"></div>
+              <div class="side-right"></div>
+              <div class="side-top"></div>
+            </div>
+            <div class="box box-4">
+              <div class="side-left"></div>
+              <div class="side-right"></div>
+              <div class="side-top"></div>
+            </div>
           </div>
         </div>
       </FadeInSection>
