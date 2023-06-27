eae19d310c942cde8514baf548c2617e719cc6d8
eruditeme <evangelineyiqin@gmail.com>
2023-06-27 18:42:45 -0400

    changed nav style

--- src/components/navi/Navi.jsx
+++ src/components/navi/Navi.jsx
@@ -7,9 +7,9 @@ import './navi.css'
 const Navi = () => {
   return (
     <div id='nav'>
-        <a href='#about' className='nav-ele'>About</a>
-        <a href='#projects' className='nav-ele'>Projects</a>
-        <a href='#contact'>Contact</a>
+        <a href='#about' className='nav-ele'>/About</a>
+        <a href='#projects' className='nav-ele'>/Projects</a>
+        <a href='#contact'>/Contact</a>
     </div>
   )
 }
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -20,26 +20,26 @@ const Project = () => {
             <ProjectCard 
               img = {easyChefGif}
               name='Easy Chef' 
-              tech='Built using Django, React, SQL, HTML, CSS, Bootstrap'
+              tech='Tech Stack: Django, React, SQL, HTML, CSS'
               github = 'https://github.com/eruditeme/EasyChef'
-              descr = 'Easy Chef is a recipe sharing web application. With Easy Chef, you can search through recipes from various diets and ingredients from all over the world, share your custom recipe with everyone, as well as filter recipe ideas based on your needs. No need to worry about your grocery list either, Easy Chef takes care of that as well!'
+              descr = 'Easy Chef is a recipe sharing web application. With Easy Chef, you can search through recipes from various diets and ingredients from all over the world, share your custom recipe, as well as filter recipe ideas based on your needs. No need to worry about your grocery list either, Easy Chef takes care of that as well!'
             />
           </FadeInSection>
           <FadeInSection>
             <ProjectCard 
               img = {todolist}
               name='To Do App' 
-              tech='Built using EJS, MongoDB, Express.js, HTML, CSS'
+              tech='Tech Stack: EJS, MongoDB, Express.js, HTML, CSS'
               github = 'https://github.com/eruditeme/toDoApp'
               demo = 'https://evening-wave-11303-56595dc7bb5c.herokuapp.com/'
-              descr = "Tired of forgetting what you need to do for the day? Well, this to do list can help you solve just that. This to-do app empowers you to tailor your to-do lists according to your unique needs and preferences. Whether or not you need a list for school, work, or home, this app lets you to customize your lists however you wish."
+              descr = "Tired of forgetting what you need to do for the day? Well, this to do list can help you solve just that. This to-do app empowers you to tailor your to-do lists according to your unique needs. Whether or not you need a list for school, work, or home, this app lets you to customize your lists however you wish."
             />
           </FadeInSection>
           <FadeInSection>
             <ProjectCard 
               img = {townImg}
               name='Town' 
-              tech='Built using HTML Canvas, Javascript'
+              tech='Tech Stack: HTML Canvas, Javascript'
               github = "https://github.com/eruditeme/Town"
               demo = 'https://papaya-cendol-bd7840.netlify.app'
               descr = 'Inspired by Pokémon, Town is a game where you, the player, can walk around a small quiet town using the WASD keys. However, in certain areas, players will have a chance of encountering a draggle. When this occurs, a battle will be initiated.'
@@ -49,10 +49,10 @@ const Project = () => {
             <ProjectCard 
               img = {weatherImg}
               name='Weatherman' 
-              tech='Built using React, OpenWeather API, HTML, CSS, Bootstrap'
+              tech='Tech Stack: React, HTML, CSS, Bootstrap'
               github = 'https://github.com/eruditeme/easy-chef'
               demo = 'https://lamevang-weather-app.netlify.app/'
-              descr = 'This weather app enables users to stay up-to-date on the current weather conditions in any city around the world. With this app, you can easily search for a city and get real-time information on the temperature, pressure, wind speed, and humidity levels.'
+              descr = 'This weather app enables users to stay up-to-date on the current weather conditions in any city around the world. With this app, you can easily search for a city and get real-time information on the temperature, wind speed, and humidity levels.'
             />
           </FadeInSection>
         </div>
